bd9da3db15dd375e41246022def06f4c50f94e5d
nair66 <aditya_nair@Feliscatus.localdomain>
2021-08-29 23:58:00 +0530

    fixed connected clients and added player roles

--- public/js/client.js
+++ public/js/client.js
@@ -1,6 +1,6 @@
 const socket = io()
 selectedChildElementsIndex = new Set()
-
+const player_role_element = document.querySelector('#player_role')
 const container = document.querySelector('#container')
 
 // console.log(container.children)
@@ -16,8 +16,23 @@ socket.on('cardSelectionUpdate',(selectedElements) => {
 })
 
 socket.on('clientCountUpdate',(count) => {
+    console.log(socket.id)
     clientCountElement = document.querySelector('#client_count')
     clientCountElement.innerHTML = `Connected Clients: ${count}`
+})
+
+socket.on('actorMessage',() => {
+    alert('You are the Actor, You can select the cards.')
+    player_role_element.innerHTML = 'Actor'
+})
+
+socket.on('guesserMessage',() => {
+    alert('You are the guesser, you must guess the answer.')
+    player_role_element.innerHTML = 'Guesser'
+})
+
+socket.on('waitMessage', () => {
+    player_role_element.innerHTML = 'Please wait for more players to join...'
 })
 
 container.addEventListener('click',(e) => {
@@ -25,7 +40,7 @@ container.addEventListener('click',(e) => {
     clickedCardItem = e.target.closest(".card")
     
     if(clickedCardItem){
-        console.log('A card was clicked')
+        // console.log('A card was clicked')
         cardItemIndex = [...clickedCardItem.parentNode.children].indexOf(clickedCardItem)
         socket.emit('cardClicked',cardItemIndex)
     }
--- src/index.js
+++ src/index.js
@@ -12,30 +12,75 @@ const publicDirPath = path.join(__dirname,'../public')
 app.use(express.static(publicDirPath))
 selectedElements = new Set([])
 connectedClientsCount = 0
+actor_id = ''
+
 
 io.on('connection',(socket) => {
 
     connectedClientsCount = io.sockets.sockets.size
     console.log('New Websocket connection')
-    // console.log(io.sockets.sockets.keys())
+    connectedClients = Array.from(io.sockets.sockets.keys())
+
+    if(connectedClientsCount == 1){
+        // console.log(actor_id)
+        io.emit('waitMessage')
+    }
+    else if(connectedClientsCount > 1 && actor_id == ''){
+        actor_id = socket.id
+        socket.emit('actorMessage')
+        socket.broadcast.emit('guesserMessage')
+    }
+    else if(actor_id != ''){
+        socket.emit('guesserMessage')
+    }
+
+
+    console.log(connectedClients)
     io.emit('clientCountUpdate',connectedClientsCount)
     socket.emit('cardSelectionUpdate',[...selectedElements])
 
     socket.on('cardClicked',(cardItemIndex) => {
-        // console.log(socket.id)
-        if(!selectedElements.has(cardItemIndex)){
+        
+        if(socket.id == actor_id){
+            // console.log(socket.id)
+            if(!selectedElements.has(cardItemIndex)){
 
-            selectedElements.add(cardItemIndex) 
-        }
-        else{
+                selectedElements.add(cardItemIndex) 
+            }
+            else{
 
-            selectedElements.delete(cardItemIndex)
+                selectedElements.delete(cardItemIndex)
+            }
+            io.emit('cardSelectionUpdate',[...selectedElements])
         }
-        io.emit('cardSelectionUpdate',[...selectedElements])
+
     })
     
     socket.on('disconnect',() => {
+        connectedClientsCount = io.sockets.sockets.size
         io.emit('clientCountUpdate',connectedClientsCount)
+
+        if(socket.id == actor_id){
+            // console.log(socket.id)
+            // console.log(actor_id)
+            actor_id = ''
+
+        if(connectedClientsCount == 1){
+            // console.log(actor_id)
+            io.emit('waitMessage')
+        }
+        else if(connectedClientsCount > 1 && actor_id == ''){
+            connectedClients = Array.from(io.sockets.sockets.keys())
+            actor_id = connectedClients[0]
+            actor_socket = io.sockets.sockets.get(actor_id)
+            actor_socket.emit('actorMessage')
+            actor_socket.broadcast.emit('guesserMessage')
+            // io.to(actor_id).broadcast.emit('guesserMessage')
+        }
+
+
+        }
+
     })
 })
 
