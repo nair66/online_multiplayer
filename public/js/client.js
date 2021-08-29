a8ee3e320c5bacdf3206f4324424762fb255f606
nair66 <aditya_nair@Feliscatus.localdomain>
2021-08-29 12:59:47 +0530

    Init Commit

---
+++ public/js/client.js
@@ -0,0 +1,39 @@
+const socket = io()
+selectedChildElementsIndex = new Set()
+
+const container = document.querySelector('#container')
+
+// console.log(container.children)
+
+// socket.on('clientInit',(selectedElements) => {
+//     selectedChildElementsIndex = new Set(selectedElements)
+//     renderCardSelections()
+// })
+
+socket.on('cardSelectionUpdate',(selectedElements) => {
+    selectedChildElementsIndex = new Set(selectedElements)
+    renderCardSelections()
+})
+
+container.addEventListener('click',(e) => {
+
+    clickedCardItem = e.target.closest(".card")
+    
+    if(clickedCardItem){
+        console.log('A card was clicked')
+        cardItemIndex = [...clickedCardItem.parentNode.children].indexOf(clickedCardItem)
+        socket.emit('cardClicked',cardItemIndex)
+    }
+})
+
+function renderCardSelections(){
+    containerChildren = document.querySelector('#container').children
+
+    for (element of containerChildren){
+        element.style.background = "white"
+    }
+
+    selectedChildElementsIndex.forEach(element => {
+        containerChildren[element].style.background = "pink"
+    });
+}
---
+++ src/index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const http = require('http')
+const path = require('path')
+const socketio = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirPath = path.join(__dirname,'../public')
+app.use(express.static(publicDirPath))
+selectedElements = new Set([])
+
+
+io.on('connection',(socket) => {
+
+    console.log('New Websocket connection')
+    console.log(io.sockets.sockets.keys())
+    socket.emit('cardSelectionUpdate',[...selectedElements])
+
+    socket.on('cardClicked',(cardItemIndex) => {
+        console.log(socket.id)
+        if(!selectedElements.has(cardItemIndex)){
+
+            selectedElements.add(cardItemIndex) 
+        }
+        else{
+
+            selectedElements.delete(cardItemIndex)
+        }
+        io.emit('cardSelectionUpdate',[...selectedElements])
+    })
+
+})
+
+
+server.listen(port, () => {
+    console.log(`server is up on port: ${port}`)
+})
